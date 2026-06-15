6a45afb39b5adde3f1a02a634562a4019784a0b9
Diogo Viegas <dviegas@c2r3s12.42lisboa.com>
2026-06-15 17:19:49 +0100

    update main

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -3,20 +3,30 @@
 
 int main()
 {
-	Bureaucrat person("John", 10);
-	Form form("01",50,20);
+    std::cout << "--- Teste 1: Sucesso ao assinar ---" << std::endl;
+    Bureaucrat john("John", 10);
+    Form formA("Formulário A", 50, 20);
 
-	std::cout << form << std::endl;
-	try
-	{
-		form.beSigned(person);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	person.signForm(form);
+    std::cout << formA << std::endl;
+    john.signForm(formA);
+    std::cout << formA << std::endl;
 
-	std::cout << form << std::endl;
-	return (0);
+    std::cout << "\n--- Teste 2: Falha por nota insuficiente ---" << std::endl;
+    Bureaucrat bob("Bob", 140);
+    Form formB("Formulário B", 50, 20);
+
+    std::cout << formB << std::endl;
+    bob.signForm(formB); 
+    std::cout << formB << std::endl;
+
+   
+    std::cout << "\n--- Teste 3: Criação de formulário inválido ---" << std::endl;
+    try {
+        Form formInvalido("Erro", 0, 155);
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Exceção apanhada: " << e.what() << std::endl;
+    }
+
+    return (0);
 }
